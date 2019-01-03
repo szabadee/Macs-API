62cf4cd7c8f586223bb9aaf0ec135326b046c1e1
Szabadi Péter <kreativ.szabadi@gmail.com>
2019-01-03 20:41:27 +0100

    final version

--- index.js
+++ index.js
@@ -7,18 +7,18 @@ const methodOverride = require('method-override');
 const handlebars = require('express-handlebars');
 
 app.use(methodOverride('_method'));
+app.use(bodyParser.urlencoded( { extended: false } ));
 
 app.engine('handlebars', handlebars({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
 app.get('/', (req, res) => {
-	models.User.findAll().then(macs => {
-		res.locals.macs = macs;
-		res.render(`macs/index.handlebars`);
+  models.Mac.findAll().then(macs => {
+    res.locals.macs = macs;
+		res.render('macs/index.handlebars');
 	});
 });
 
-app.use(bodyParser.urlencoded( { extended: false } ));
 app.use('/macs', macs);
 
 app.listen(3000);
